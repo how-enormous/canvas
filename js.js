523d6210b99452fab082004e26a82b6d781f7bae
how-enormous <59827483+how-enormous@users.noreply.github.com>
2020-12-06 14:16:42 +0800

    TwoBall
    imperfect method

--- js.js
+++ js.js
@@ -21,22 +21,40 @@ function car(wheel,width,height){
     this.width = width;
     this.height = height;
 }
-var dx=2.5;
-var dy=1.8;
-function onTimeout(){
-    
-    if(x+2>=350||x+2<=50)
-    dx=-dx;
-    if(y+2>=350||y+2<=50)
-    dy=-dy;
-    x+=dx;
-    y+=dy;
+var dx1=2.5;
+var dy1=1.8;
+var dx2=2.5;
+var dy2=2;
+var dis = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+function onTimeout1(){
+     dis = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+    if(x1+2>=350||x1+2<=50||dis<=10000)
+    dx1=-dx1;
+    if(y1+2>=350||y1+2<=50||dis<=10000)
+    dy1=-dy1;
+    x1+=dx1;
+    y1+=dy1;
     ctx.clearRect(0,0,400,400)
     ctx.beginPath();
-    ctx.arc(x,y,50,0,Math.PI*2,false);
+    ctx.arc(x1,y1,50,0,Math.PI*2,false);
     ctx.stroke();
     ctx.fillStyle='#ff04ff';
     ctx.fill();
+    onTimeout2();
+};function onTimeout2(){
+    
+    if(x2+2>=350||x2+2<=50||dis<=10000)
+    dx2=-dx2;
+    if(y2+2>=350||y2+2<=50||dis<=10000)
+    dy2=-dy2;
+    x2+=dx2;
+    y2+=dy2;
+    ctx.beginPath();
+    ctx.arc(x2,y2,50,0,Math.PI*2,false);
+    ctx.stroke();
+    ctx.fillStyle='#00a4cf';
+    ctx.fill();
 };
-var x=50,y=100;
-var timer = setInterval(onTimeout,50);
+var x1=50,y1=100;
+var x2=170,y2=300;
+var timer = setInterval(onTimeout1,50);
